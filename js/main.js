753d473b779c846869c3dde604c4225d4422c04f
luntic-z <68658158+luntic-z@users.noreply.github.com>
2022-02-14 01:25:12 +0300

    Добавила адаптив слайдера под мобильные устройства

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.main-slider', {
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 20,
   keyboard: {
     enabled: true
@@ -9,4 +9,10 @@ const swiper = new Swiper('.main-slider', {
   navigation: {
     nextEl: '.main-slider__arrow',
   },
+  breakpoints: {
+    // when window width is >= 376px
+    376: {
+      slidesPerView: 2,
+    },
+  }
 });
