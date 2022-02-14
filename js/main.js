16720ab0aa19a8a6df2ac3ea430317abcb605527
luntic-z <68658158+luntic-z@users.noreply.github.com>
2022-02-14 22:25:17 +0300

    добавлены видео, адаптив под мобильные устройства

--- js/main.js
+++ js/main.js
@@ -10,9 +10,36 @@ const swiper = new Swiper('.main-slider', {
     nextEl: '.main-slider__arrow',
   },
   breakpoints: {
-    // when window width is >= 376px
-    376: {
+    // when window width is >= 501px
+    769: {
       slidesPerView: 2,
     },
   }
 });
+
+let burger = document.querySelector('.menu-burger');
+let closeMenu = document.querySelector('.header__menu-close');
+let menuMobile = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+  menuMobile.style.display = 'block';
+})
+
+closeMenu.addEventListener('click', () => {
+  menuMobile.style.display = 'none';
+})
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main__button-play');
+
+buttonModal.addEventListener('click', (e) => {
+  modalWindow.classList.add('active');
+})
+
+modalWindow.addEventListener('click', (e) => {
+  const isModal = e.target.closest('.modal__inner');
+
+  if (!isModal) {
+    modalWindow.classList.remove('active');
+  }
+})
